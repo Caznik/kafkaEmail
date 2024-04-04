d9010e7a66d3f0b051a4861e4fdcdef46c5a86a5
Carlos González González <carlosglez.28695@gmail.com>
2024-04-04 08:41:21 +0200

    Simplified spring boot scan package

--- kafka/src/main/java/com/example/web/KafkaApplication.java
+++ kafka/src/main/java/com/example/web/KafkaApplication.java
@@ -2,8 +2,10 @@ package com.example.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"com.example.controller", "com.example.service", "com.example.configuration"})
+@SpringBootApplication
+@ComponentScan(basePackages = "com.example")
 public class KafkaApplication {
 
 	public static void main(String[] args) {
