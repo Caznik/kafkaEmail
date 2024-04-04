1e54abda46303d99f6274761af390e4fedb3c889
Carlos González González <carlosglez.28695@gmail.com>
2024-04-04 08:40:49 +0200

    Improved post message to topic with asynchronous result managing

--- kafka/src/main/java/com/example/service/KafkaMessageServiceImpl.java
+++ kafka/src/main/java/com/example/service/KafkaMessageServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.service;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import com.example.model.KafkaMessage;
@@ -14,7 +17,16 @@ public class KafkaMessageServiceImpl implements KafkaMessageService{
 	
 	@Override
 	public void createMessage(KafkaMessage kafkaMessage) {
-		kafkaTemplate.send(kafkaMessage.getTopic(), kafkaMessage.getMessage());
+		//kafkaTemplate.send(kafkaMessage.getTopic(), kafkaMessage.getMessage());
+		
+		CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(kafkaMessage.getTopic(), kafkaMessage.getMessage());
+	    future.whenComplete((result, ex) -> {
+	        if (ex == null) {
+	            System.out.println("Sent message=[" + kafkaMessage.getMessage() + "] with offset=[" + result.getRecordMetadata().offset() + "]");
+	        } else {
+	            System.out.println("Unable to send message=[" + kafkaMessage.getMessage() + "] due to : " + ex.getMessage());
+	        }
+	    });
 	}
 
 	@Override
